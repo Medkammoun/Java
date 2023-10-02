73fe092f7d4ecb58f1a6d5ea8cc7c7de73918813
Mohamed Kammoun <136347204+Medkammoun@users.noreply.github.com>
2023-10-02 18:33:54 +0100

    Commit

---
+++ Week 1/Day 1/First_java_program/First_java_program.java
@@ -0,0 +1,9 @@
+public class First_java_program {
+    public static void main(String[] args) {
+
+        System.out.println("Mohamed Kammoun");
+        System.out.println("31 yers old");
+        System.out.println("My hometown Tunisia");
+
+    }
+}
